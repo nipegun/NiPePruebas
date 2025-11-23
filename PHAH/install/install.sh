0bfa8d43530c23b053a70b2792bf62c7f7b38db8
NiPeGun <nipegun@gmail.com>
2025-11-23 01:42:30 +0100

    Update install.sh

--- PHAH/install/install.sh
+++ PHAH/install/install.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
 # Definir constantes
-  vNomnreRepoGithub='pruebas'
-  vUbicArchEnRepo='PHAP'
+  vNombreRepoGithub='pruebas'
+  vUbicArchEnRepo='PHAH'
 
 # Clonar el repo de pruebas
   cd /tmp
-  rm -rf /tmp/pruebas/
+  rm -rf /tmp/"$vNombreRepoGithub"/
   git clone https://github.com/nipegun/"$vNombreRepoGithub".git
 
 # Crear carpeta de hacking en la home del usuario
-  mkdir -p $HOME/HackingTools/PHAH/
+  mkdir -p $HOME/HackingTools/PHAH/ 2> /dev/null
+  rm -rf $HOME/HackingTools/PHAH/* 2> /dev/null
 
 # Mover archivos a la carpeta
   cp -Rv /tmp/"$vNombreRepoGithub"/"$vUbicArchEnRepo"/* $HOME/HackingTools/PHAH/
+
+# Crear el entorno virtual de python
+  cd $HOME/HackingTools/PHAH/
+  python3 -m venv venv
+
+# Entrar en el entorno virtual e instalar requirements
+  source $HOME/HackingTools/PHAH/venv/bin/activate
+  pip install -r install/requirements.txt
+
+# Salir del entorno virtual
+  deactivate
+
+# Notificar fin de la instalación
+  echo ""
+
