a26fb7e96119ad42684cbe19318d38c2172d5fd4
NiPeGun <nipegun@gmail.com>
2025-12-01 02:33:02 +0100

    Create 05-SecurityMisconfiguration.py

---
+++ OWASPTop10/2025/05-SecurityMisconfiguration.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def fIndex():
+  # VULN: Exposición de variables de entorno
+  return f"<pre>{os.environ}</pre>"
+
+if __name__ == "__main__":
+  # Debug + información sensible
+  app.run(host="0.0.0.0", port=5005, debug=True)
